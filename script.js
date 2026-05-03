fcafa34fa2e1c42ebcd941a5331929e5dc9ea0f1
Hasitha <hasithawijesinghe2020@gmail.com>
2026-05-03 20:51:14 +0530

    update EmailJS configuration and refactor contact form submission logic

--- script.js
+++ script.js
@@ -37,30 +37,11 @@ console.log('Script loaded successfully');
 console.log('All sections:', document.querySelectorAll('.visited-section'));
 
 
-// how to navigate another webside in new tab
-
-// 1. using href
-// 2. using window.open
-// 3. using window.location
-
+// open new tab function
 function openNewTab(url) {
   window.open(url, '_blank');
 }
 
-// i want to open pdf file to download in current tab not new tab
-//no i want full screen view of pdf file in current tab and i want to download pdf file when click
-
-// for download the file 
-// download pdf file function in current tab 
-
-
-// for open the file in new tab
-
-// open pdf file function in new tab
-function openCVInNewTab() {
-  window.open('assets/Hasitha Wijesinghe CV-26-01.pdf', '_blank');
-  // document.title = "Hasitha Wijesinghe CV";
-}
 
 function validateContactForm() {
   const name = document.getElementById('name').value;
@@ -80,33 +61,37 @@ function validateContactForm() {
   }
 }
 
-function validateEmail(email) {
-  const emailRegex = /[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
+
 
 function sendEmail() {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
   if (validateContactForm()) {
-    const serviceID = "....";
-    const templateID = "template_tomvhrs";
+    const serviceID = "service_ulcjp0h";
+    const templateID = "template_s30y7dc";
 
-    emailjs.send(serviceID, templateID, {
-      from_name: name,
+    const params = {
+      name: name,
       from_email: email,
-      to_name: "Hasitha",
+      from_name: 'Personal Portfolio Website',
       message: message,
-      title: name + " from " + email,
-    })
+      title: message.substring(0, 12) + "...",
+      time: new Date().toLocaleString(),
+    }
+    emailjs.send(serviceID, templateID, params)
       .then(() => {
         alert("Email sent successfully!");
       })
       .catch((error) => {
         console.error("Error sending email:", error);
       });
+
+
   }
+  document.getElementById('name').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('message').value = '';
 }
 
 
